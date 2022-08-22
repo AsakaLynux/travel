4cfb06f65fedae7c0a82b271a43c30efb43d4c92
AsakaLynux <asakalynux@gmail.com>
2022-08-22 10:22:43 +0700

    detail page - title

---
+++ lib/ui/pages/detail_page.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import '../../shared/theme.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget backgroundImage() {
+      return Container(
+        width: double.infinity,
+        height: 450,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/image_destination_1.png'),
+          ),
+        ),
+      );
+    }
+
+    Widget customShadow() {
+      return Container(
+        margin: EdgeInsets.only(top: 236),
+        height: 214,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              kWhiteColor.withOpacity(0),
+              Colors.black.withOpacity(0.95),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget content() {
+      return Container(
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+        child: Column(
+          children: [
+            // Emblem
+            Container(
+              width: 108,
+              height: 24,
+              margin: EdgeInsets.only(
+                top: 30,
+              ),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/image_emblem.png'),
+                ),
+              ),
+            ),
+
+            // Title
+            Container(
+              margin: EdgeInsets.only(top: 256),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Lake Ciliwung',
+                          style: whiteTextStyle.copyWith(
+                            fontSize: 24,
+                            fontWeight: semiBold,
+                          ),
+                        ),
+                        Text(
+                          'Tangerang',
+                          style: greyTextStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: ligth,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 2),
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/icon_star.png'),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '4.8',
+                        style: whiteTextStyle.copyWith(
+                          fontWeight: medium,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: kbackgroundColor,
+      body: Stack(
+        children: [
+          backgroundImage(),
+          customShadow(),
+          content(),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/widget/destination_card.dart
+++ lib/ui/widget/destination_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel/ui/pages/detail_page.dart';
 import '../../shared/theme.dart';
 
 class DestinationCard extends StatelessWidget {
@@ -17,85 +18,95 @@ class DestinationCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      height: 323,
-      margin: EdgeInsets.only(left: defaultMargin),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: kWhiteColor,
-        borderRadius: BorderRadius.circular(18),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: 180,
-            height: 220,
-            margin: EdgeInsets.only(bottom: 20),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(imageUrl),
-              ),
-              borderRadius: BorderRadius.circular(18),
-            ),
-            child: Align(
-              alignment: Alignment.topRight,
-              child: Container(
-                height: 30,
-                width: 55,
-                decoration: BoxDecoration(
-                  color: kWhiteColor,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(18),
-                  ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailPage(),
+          ),
+        );
+      },
+      child: Container(
+        width: 200,
+        height: 323,
+        margin: EdgeInsets.only(left: defaultMargin),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.circular(18),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 180,
+              height: 220,
+              margin: EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(imageUrl),
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(right: 2),
-                      width: 20,
-                      height: 20,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage('assets/icon_star.png'),
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  height: 30,
+                  width: 55,
+                  decoration: BoxDecoration(
+                    color: kWhiteColor,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(18),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 2),
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/icon_star.png'),
+                          ),
                         ),
                       ),
-                    ),
-                    Text(
-                      rating.toString(),
-                      style: blackTextStyle.copyWith(
-                        fontWeight: medium,
+                      Text(
+                        rating.toString(),
+                        style: blackTextStyle.copyWith(
+                          fontWeight: medium,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: blackTextStyle.copyWith(
-                    fontSize: 18,
-                    fontWeight: medium,
+            Container(
+              margin: EdgeInsets.only(left: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: blackTextStyle.copyWith(
+                      fontSize: 18,
+                      fontWeight: medium,
+                    ),
                   ),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  city,
-                  style: greyTextStyle.copyWith(fontWeight: ligth),
-                ),
-              ],
+                  SizedBox(height: 5),
+                  Text(
+                    city,
+                    style: greyTextStyle.copyWith(fontWeight: ligth),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
